dc88158332908c50bc977c82cc36c9ffd0f659a6
cryptoseabook <cryptoseabook@gmail.com>
2021-07-20 21:39:44 +1000

    add more rpc urls

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,11 +1,11 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3, process.env.REACT_APP_NODE_4]
+const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2]
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
-  return nodes[randomIndex]
+  return nodes[0]
 }
 
 export default getNodeUrl
