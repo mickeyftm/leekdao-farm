9e348a7a7b4fb70cd7d878d2d4090a5f95f8f216
Shuo Wang <shuowang@192-168-1-100.tpgi.com.au>
2021-08-10 15:28:27 +1000

    add more locations

---
+++ node_modules/leek-uikit/dist/widgets/WalletModal/WrongNetworkModal.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+interface Props {
+    onDismiss?: () => void;
+    logout: () => void;
+}
+declare const WrongNetworkModal: React.FC<Props>;
+export default WrongNetworkModal;
